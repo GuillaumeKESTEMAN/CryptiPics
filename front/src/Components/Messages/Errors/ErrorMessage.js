94d2d54d00d7081b82611049c2a2b3a83f1a03b1
Nekewo <julessorens@gmail.com>
2022-03-05 18:54:19 +0100

    edit front
    Regarde les changements si tu veux tout savoir non ? .. xd

--- front/src/Components/Messages/Errors/ErrorMessage.js
+++ front/src/Components/Messages/Errors/ErrorMessage.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 
+/**
+ * @param {object} props Objet contenant les propriétés
+ * @param {string | undefined} props.message Message unique
+ * @param {array<string> | undefined} props.messages Tableau de plusieurs messages
+ */
 function ErrorMessage(props) {
 
 
     return (
         <>
-            <p style={{color: "red"}}>{props.message}</p>
-        </>
+            {props.message &&
+                <p className='text-red-500'>{props.message}</p>
+            }
+            {
+                !props.message &&
+                <div className='bg-white rounded p-1'>
+                    {props.messages.map((msg) => {
+                        <p className='text-red-500'>{msg}</p>
+                    })}
+                </div>
+            }
+            </>
     );
 }
 
